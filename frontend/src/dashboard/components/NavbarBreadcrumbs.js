0b4e04a0ff1acd8f479b73eef84a4d32d836a1f8
Imaneghanem <imane.ghanem@bordeaux-inp.fr>
2024-11-07 11:59:56 +0100

    salut §§

--- frontend/src/dashboard/components/NavbarBreadcrumbs.js
+++ frontend/src/dashboard/components/NavbarBreadcrumbs.js
@@ -14,7 +14,7 @@ const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
     alignItems: 'center',
   },
 }));
-//salut
+//salut hshshs
 export default function NavbarBreadcrumbs() {
   return (
     <StyledBreadcrumbs
